6afceab2e6c857e41dc0fe1ab40e0a472aec4068
AmazoiEmre <amazoisftwr@gmail.com>
2022-03-10 14:14:07 +0300

    şş

--- public/client.js
+++ public/client.js
@@ -8,135 +8,97 @@ window.addEventListener('load', function() {
 		
 		var args = Array.prototype.slice.call(arguments, 1);
 		
-		if(window.unityInstance!=null)
+		f(window.unityInstance!=null)
 		{
+		  //fit formats the message to send to the Unity client game, take a look in NetworkManager.cs in Unity
 		  window.unityInstance.SendMessage("NetworkManager", method, args.join(':'));
+		
 		}
 		
-		
-	};
+	};//END_exe_In_Unity 
 
 	
-	socket.on('JOIN_SUCCESS', function(id,name,position,total_players) {
+	socket.on('PONG', function(socket_id,msg) {
 				      		
-	  var currentUserAtr = id+':'+name+':'+position+':'+total_players;
+	  var currentUserAtr = socket_id+':'+msg;
+	  
 	 if(window.unityInstance!=null)
 		{
-		 window.unityInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
-		}
-	  
-	});
-	
-	socket.on('RESPAWN_PLAYER', function(id,name,position) {
-				      		
-	  var currentUserAtr = id+':'+name+':'+position;
-	 if(window.unityInstance!=null)
-		{
-		 window.unityInstance.SendMessage ('NetworkManager', 'OnRespawPlayer', currentUserAtr);
-		}
-	  
-	});
-	socket.on('SPAWN_PLAYER', function(id,name,position,total_players) {
-				      		
-	  var currentUserAtr = id+':'+name+':'+position+':'+total_players;
-	   if(window.unityInstance!=null)
-		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
-		}
-	 
-	});
-	
-	socket.on('UPDATE_POS_AND_ROT', function(id,position,rotation) {
-				      		
-	  var currentUserAtr = id+':'+position+":"+rotation;
-	  
-	  if(window.unityInstance!=null)
-		{
-		 window.unityInstance.SendMessage ('NetworkManager', 'OnUpdatePosAndRot', currentUserAtr);
-		}
-	 
-	});
-	
-	 socket.on('UPDATE_PLAYER_ANIMATOR', function(id,animation) {
-	 
-	     var currentUserAtr = id+':'+animation;
+		 
+		  window.unityInstance.SendMessage ('NetworkManager', 'OnPrintPongMsg', currentUserAtr);
 		
+		}
+	  
+	});//END_SOCKET.ON
+	
+			
+    socket.on('ON_UPDATE_SOUND', function(id,name) {
+	     var currentUserAtr = id+':'+name;
+		 	
 		 if(window.unityInstance!=null)
 		{
-		  
-		   // sends the package currentUserAtr to the method OnUpdateAnim in the NetworkManager class on Unity 
-		   window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateAnim',currentUserAtr);
+		   window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateSound',currentUserAtr);
 		}
 		
 	});//END_SOCKET.ON
 	
-	socket.on('UPDATE_BEST_KILLER', function(name,ranking,kills) {
+
+					      
+	socket.on('LOGIN_SUCCESS', function(id,name,position) {
 				      		
-	  var currentUserAtr = name+":"+ranking+":"+kills;
+	  var currentUserAtr = id+':'+name+':'+position;
+	  
 	   if(window.unityInstance!=null)
 		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateBestKillers', currentUserAtr);
-		}
-	 
-	});
-	
-	 socket.on('UPDATE_BUSTER', function(id) {
-				      		
-	  var currentUserAtr = id+":"+"";
-	  
-	  if(window.unityInstance!=null)
-		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateBuster', currentUserAtr);
-		}
-	 
-	});	
-	
-	
-
-	  socket.on('UPDATE_SHOOT', function(id,target) {
-				      		
-	  var currentUserAtr = id+":"+target;
-	  
-	  if(window.unityInstance!=null)
-		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateShoot', currentUserAtr);
-		}
-	 
-	});	
-	
-   socket.on('UPDATE_PLAYER_DAMAGE', function(shooterID,targetID,health) {
-				      		
-	  var currentUserAtr = shooterID+':'+targetID+':'+health;
-	  if(window.unityInstance!=null)
-		{
-		 window.unityInstance.SendMessage ('NetworkManager', 'OnUpdatePlayerDamage', currentUserAtr);
+		 
+		  window.unityInstance.SendMessage ('NetworkManager', 'OnJoinGame', currentUserAtr);
+		
 		}
 	  
-	});	
+	});//END_SOCKET.ON
 	
-	socket.on('DEATH', function(id) {
-				      		
-	  var currentUserAtr = id+":"+"";
-	  
-	  if(window.unityInstance!=null)
+		
+	socket.on('SPAWN_PLAYER', function(id,name,position) {
+	
+	    var currentUserAtr = id+':'+name+':'+position;
+		
+		if(window.unityInstance!=null)
 		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnPlayerDeath', currentUserAtr);
+	     // sends the package currentUserAtr to the method OnSpawnPlayer in the NetworkManager class on Unity
+		  window.unityInstance.SendMessage ('NetworkManager', 'OnSpawnPlayer', currentUserAtr);
+		
 		}
-	 
-	});	
-
+		
+	});//END_SOCKET.ON
 	
 
- socket.on('USER_DISCONNECTED', function(id) {
-				      		
-	  var currentUserAtr = id+":"+" ";
-	  if(window.unityInstance!=null)
+	
+    socket.on('UPDATE_MOVE_AND_ROTATE', function(id,position,rotation) {
+	     var currentUserAtr = id+':'+position+':'+rotation;
+		 	
+		 if(window.unityInstance!=null)
 		{
-		  window.unityInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
+		   window.unityInstance.SendMessage ('NetworkManager', 'OnUpdateMoveAndRotate',currentUserAtr);
 		}
-	 
-	});		
+		
+	});//END_SOCKET.ON
+	
+		        
+	socket.on('USER_DISCONNECTED', function(id) {
+	
+	     var currentUserAtr = id;
+		 
+		if(window.unityInstance!=null)
+		{
+		  
+		 window.unityInstance.SendMessage ('NetworkManager', 'OnUserDisconnected', currentUserAtr);
+		
+		
+		}
+		 
+	
+	});//END_SOCKET.ON
+	
 
-
-});//END_WINDOW.ADDEVENTLISTENER
+});//END_window_addEventListener
 
